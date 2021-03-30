99cbab73610d302b8f2bdc7e45b0124447997c22
massive2351 <massive2351@gmail.com>
2021-03-30 13:56:26 +0900

    finsh

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
   resources :users, only: [:show, :index]
+  resources :reactions, only: [:create]
+  resources :matching, only: [:index]
+  resources :chat_rooms, only: [:create, :show]
+  
 end
